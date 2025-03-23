c10592fd70837c56cefd358173a161d3b4bdd8b6
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-03-23 19:38:16 +0300

    update users table

--- services/backend/src/core/models/user.py
+++ services/backend/src/core/models/user.py
@@ -1,8 +1,15 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Enum
+from sqlalchemy_utils import EmailType
+import enum
 
+class UserRole(enum.Enum):
+    PARTICIPANT = "participant"
+    CREATOR = "creator"
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(40), unique=True)
-    # password, email/phone,
+    hashed_password: Mapped[str] = mapped_column(String(128))
+    email: Mapped[str] = mapped_column(EmailType, unique=True)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole))
