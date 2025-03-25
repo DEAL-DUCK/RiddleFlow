220a0b7e92529ee76b6efecc7feddc536f794f91
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-03-25 18:21:04 +0300

    Add new API for hackathons

--- services/backend/src/api_v1/hackathons/crud.py
+++ services/backend/src/api_v1/hackathons/crud.py
@@ -1,8 +1,11 @@
+from fastapi import HTTPException, status
 from sqlalchemy import select, Result
+from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from services.backend.src.api_v1.hackathons.schemas import HackathonCreateSchema
-from services.backend.src.core.models import Hackathon
+from services.backend.src.api_v1.hackathons.schemas import (
+    HackathonCreateSchema,
+)
+from services.backend.src.core.models import Hackathon, User
 
 
 async def get_hackathons(session: AsyncSession) -> list[Hackathon]:
@@ -23,3 +26,55 @@ async def create_hackathon(
     session.add(hackathon)
     await session.commit()
     return hackathon
+
+
+async def add_user_in_hackathon(
+    session: AsyncSession,
+    hackathon: Hackathon,
+    user: User,
+):
+    hackathon = await session.scalar(
+        select(Hackathon)
+        .where(Hackathon.id == hackathon.id)
+        .options(selectinload(Hackathon.users)),
+    )
+    if user not in hackathon.users:
+        hackathon.users.append(user)
+        await session.commit()
+        return hackathon.users
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"User {user.id} is already participating in this hackathon",
+    )
+
+
+async def delete_user_in_hackathon(
+    session: AsyncSession,
+    hackathon: Hackathon,
+    user: User,
+):
+    hackathon = await session.scalar(
+        select(Hackathon)
+        .where(Hackathon.id == hackathon.id)
+        .options(selectinload(Hackathon.users)),
+    )
+    if user in hackathon.users:
+        hackathon.users.remove(user)
+        await session.commit()
+        return hackathon.users
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User {user.id} is not participating in this hackathon",
+    )
+
+
+async def get_users_in_hackathon(
+    session: AsyncSession,
+    hackathon: Hackathon,
+):
+    hackathon = await session.scalar(
+        select(Hackathon)
+        .where(Hackathon.id == hackathon.id)
+        .options(selectinload(Hackathon.users)),
+    )
+    return hackathon.users
--- services/backend/src/api_v1/hackathons/views.py
+++ services/backend/src/api_v1/hackathons/views.py
@@ -1,13 +1,14 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import crud
 from services.backend.src.api_v1.hackathons.dependencies import get_hackathon_by_id
+from services.backend.src.api_v1.users.dependencies import get_user_by_id
 from services.backend.src.api_v1.hackathons.schemas import (
     HackathonSchema,
     HackathonCreateSchema,
 )
-from services.backend.src.core.models import db_helper
+from services.backend.src.core.models import db_helper, User, Hackathon
 
 router = APIRouter(tags=["Хакатоны"])
 
@@ -24,9 +25,39 @@ async def get_hackathon(hackathon: HackathonSchema = Depends(get_hackathon_by_id
     return hackathon
 
 
+@router.get("/{hackathon_id}/users")
+async def get_users_in_hackathon(
+    hackathon: Hackathon = Depends(get_hackathon_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.get_users_in_hackathon(session=session, hackathon=hackathon)
+
+
 @router.post("/", response_model=HackathonSchema)
 async def create_hackathon(
     hackathon_in: HackathonCreateSchema,
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.create_hackathon(session=session, hackathon_in=hackathon_in)
+
+
+@router.post("/{hackathon_id}")
+async def add_user_in_hackathon(
+    hackathon: Hackathon = Depends(get_hackathon_by_id),
+    user: User = Depends(get_user_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.add_user_in_hackathon(
+        hackathon=hackathon, user=user, session=session
+    )
+
+
+@router.delete("/{hackathon_id}")
+async def delete_user_in_hackathon(
+    hackathon: Hackathon = Depends(get_hackathon_by_id),
+    user: User = Depends(get_user_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.delete_user_in_hackathon(
+        hackathon=hackathon, user=user, session=session
+    )
