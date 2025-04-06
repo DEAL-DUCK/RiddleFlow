bcf58eb5d442ade4c716ad7041ca6c0381201c74
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-04-06 22:49:06 +0300

    Update db_helper.py

--- services/backend/src/core/models/db_helper.py
+++ services/backend/src/core/models/db_helper.py
@@ -5,22 +5,33 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
     async_scoped_session,
+    AsyncEngine,
 )
 
 from ..config import settings
 
 
 class DatabaseHelper:
-    def __init__(self, url: str, echo: bool = False):
-        self.engine = create_async_engine(
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 5,
+        pool_timeout: int = 30,
+        pool_recycle: int = 3600,
+    ):
+        self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
-            pool_size=20,
-            max_overflow=10,
-            pool_timeout=30,
-            pool_recycle=3600,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
+            pool_timeout=pool_timeout,
+            pool_recycle=pool_recycle,
         )
-        self.session_factory = async_sessionmaker(
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
@@ -34,18 +45,20 @@ class DatabaseHelper:
         )
         return session
 
-    async def session_dependency(self) -> AsyncSession:
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncSession:
         async with self.session_factory() as session:
             yield session
-            await session.close()
-
-    async def scoped_session_dependency(self) -> AsyncSession:
-        session = self.get_scoped_session()
-        yield session
-        await session.close()
 
 
 db_helper = DatabaseHelper(
-    url=settings.db.url,
+    url=str(settings.db.url),
     echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    max_overflow=settings.db.max_overflow,
+    pool_size=settings.db.pool_size,
+    pool_timeout=settings.db.pool_timeout,
+    pool_recycle=settings.db.pool_recycle,
 )
