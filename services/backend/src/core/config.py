11811ca97fda546aa5c00732f4b723f65f2b592e
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-04-06 22:25:20 +0300

    Update settings

--- services/backend/src/core/config.py
+++ services/backend/src/core/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
@@ -7,14 +7,23 @@ BASE_DIR = Path(__file__).parent.parent
 DB_PATH = BASE_DIR / "db.sqlite3"
 
 
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
 class DbSettings(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
-    echo: bool = False
+    url: PostgresDsn
 
 
 class Settings(BaseSettings):
-    db: DbSettings = DbSettings()
-    # db_echo: bool = True
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DbSettings
 
 
 settings = Settings()
