17c6a37b4e44145f1672ebed70874e8cdf386bb4
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-04-07 23:53:12 +0300

    create auth router

--- services/backend/src/api_v1/__init__.py
+++ services/backend/src/api_v1/__init__.py
@@ -4,9 +4,12 @@ from .hackathons.views import router as hackathons_router
 # from .olympiads.views import router as olympiads_router
 from .users.views import router as users_router
 from .profiles.views import router as profiles_router
+from .auth.views import router as auth_router
+
 
 router = APIRouter(prefix="/v1")
 router.include_router(router=users_router, prefix="/users")
 router.include_router(router=profiles_router, prefix="/profiles")
 router.include_router(router=hackathons_router, prefix="/hackathons")
 # router.include_router(router=olympiads_router, prefix="/olympiads")
+router.include_router(router=auth_router, prefix="auth")
