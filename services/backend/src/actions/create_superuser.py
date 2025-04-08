cc8f08e83f99fa87ccc1c99fedcda035f4c37fc6
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-04-08 14:08:38 +0300

    Add create profile when create superuser

--- services/backend/src/actions/create_superuser.py
+++ services/backend/src/actions/create_superuser.py
@@ -1,10 +1,12 @@
 import asyncio
 import contextlib
 
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.util import await_only
 
 from core.authentication.user_manager import UserManager
-from core.models import db_helper, User
+from core.models import db_helper, User, Profile
 from api_v1.dependencies.authentication.users import get_users_db
 from api_v1.users.schemas import UserCreate
 from api_v1.dependencies.authentication.user_manager import get_user_manager
@@ -56,10 +58,14 @@ async def create_superuser(
     async with db_helper.session_factory() as session:
         async with get_users_db_context(session) as users_db:
             async with get_user_manager_context(users_db) as user_manager:
-                return await create_user(
+                user = await create_user(
                     user_manager,
                     user_create,
                 )
+                profile = Profile(user_id=user.id)
+                session.add(profile)
+                await session.commit()
+                return user
 
 
 if __name__ == "__main__":
