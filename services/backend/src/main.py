2b2de8579048bfa6d6835cc5364f8bb68647ca58
Dmitry Bazhenov <BazhenovStudy@yandex.ru>
2025-04-08 14:52:10 +0300

    preparing for containerization

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -19,10 +19,10 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api.prefix)
 
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host=settings.run.host,
-        port=settings.run.port,
-        reload=True,
-    )
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "main:app",
+#         host=settings.run.host,
+#         port=settings.run.port,
+#         reload=True,
+#     )
